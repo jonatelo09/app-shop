c4d674a4db6bcaa26098c7395d0b5dcbf134b3a2
unknown <kuroko.arevalo@gmail.com>
2019-10-10 23:57:50 -0500

    Correccion de tamano de img productos pixeles

--- resources/views/categories/showdos.blade.php
+++ resources/views/categories/showdos.blade.php
@@ -17,7 +17,7 @@
             <div class="product-item">
             	<a href="{{url('/products-dos/'.$product->id) }}">
 	              <figure>
-	                <img src="{{ $product->featured_image_url }}" alt="Image" class="img-fluid" style="width: 450px; height: 300;">
+	                <img src="{{ $product->featured_image_url }}" alt="Image" class="img-fluid" style="width: 450px; height: 300px;">
 	              </figure>
               	</a>
               <div class="px-4">
